1c461b02b5d380088c630b8d3ddf3a7705ec1a63
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-05-05 16:01:34 +0300

    style: add background image to header and fix layout for better display

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "./api/axiosInstance";
+import Logo from "./assets/Logo.svg";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [countries, setCountries] = useState([]);
 
   const fetchData = async () => {
     try {
       const response = await axios.get("all?sort=population");
-      console.log(response.data);
+      const data = response.data;
     } catch (e) {
       console.error(e.message);
     }
@@ -19,7 +20,19 @@ function App() {
     fetchData();
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <div className="bg-[#1B1D1F]">
+        <main className="min-h-screen flex ">
+          <header className="header flex justify-center items-center w-full">
+            <img src={Logo} alt="logo" className="" />
+          </header>
+
+          <div className=""></div>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
