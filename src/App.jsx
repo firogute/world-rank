574ce1b3341fe2b3dc38cdb9962057e656d8ab37
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-05-05 16:57:19 +0300

    feat: add search bar and sort dropdown UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "./api/axiosInstance";
 import Logo from "./assets/Logo.svg";
+import SearchIcon from "./assets/Search.svg";
 
 import "./App.css";
 
@@ -23,12 +24,52 @@ function App() {
   return (
     <>
       <div className="bg-[#1B1D1F]">
-        <main className="min-h-screen flex ">
+        <main className="min-h-screen flex flex-col font-['Be_Vietnam_Pro',sans-serif] font-normal">
           <header className="header flex justify-center items-center w-full">
             <img src={Logo} alt="logo" className="" />
           </header>
 
-          <div className=""></div>
+          <div className="z-10 flex justify-center">
+            <div className="text-[#D2D5DA] bg-[#1B1D1F] border border-[#282B30] -mt-[60px] mx-3 rounded-xl py-6 px-3 md:px-7 md:mx-7 basis-7xl">
+              <div className="grid grid-cols-1 md:grid-cols-[1fr_384px]  gap-6">
+                <span>Found 100 Countries</span>
+                <div className="relative">
+                  <input
+                    type="text"
+                    className="cursor-text placeholder:text-[#D2D5DA] rounded-xl bg-[#282B30] pl-11 px-3 py-2.5 w-full"
+                    placeholder="Search by Name, Region, Subregion"
+                  />
+                  <img
+                    src={SearchIcon}
+                    alt="search-icon"
+                    className="absolute top-1/2 -translate-y-1/2 left-2 size-6"
+                  />
+                </div>
+              </div>
+              <div className="mt-9 grid grid-cols-1 md:grid-cols-[215px_1fr] gap-7">
+                <div className="font-medium flex flex-col gap-3">
+                  <label htmlFor="sortby" className="text-xs">
+                    Sort By
+                  </label>
+                  <select
+                    name="sortby"
+                    id=""
+                    className="cursor-pointer px-3 py-2 pr-8 border-2 border-[#282B30] rounded-xl appearance-none bg-no-repeat bg-[right_0.5rem_center] text-sm"
+                  >
+                    <option value="area" className="bg-[#1B1D1F]">
+                      Area
+                    </option>
+                    <option value="name" className="bg-[#1B1D1F]">
+                      Name
+                    </option>
+                    <option value="population" className="bg-[#1B1D1F]">
+                      Population
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
       </div>
     </>
