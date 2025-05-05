7b86615d4576d33b7bd14a5222864dca5be9b907
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-05-05 14:58:34 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://restcountries.com/v3.1/all?sort=population"
+      );
+
+      const data = await response.json();
+      console.log(data[0]);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+  useEffect(() => {
+    console.log("Fetching");
+    fetchData();
+  }, []);
+
+  return <></>;
+}
+
+export default App;
