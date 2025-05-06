55d4a99dc4e262488e16362999c4291dd0d54651
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-05-06 06:12:22 +0300

    feat: add client-side pagination with filtering logic for countries list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios from "./api/axiosInstance";
 import Logo from "./assets/Logo.svg";
 import SearchIcon from "./assets/Search.svg";
@@ -7,12 +7,15 @@ import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
   const fetchData = async () => {
     try {
       const response = await axios.get("all?sort=population");
       const data = response.data;
-      setCountries(response.data);
+      console.log(data);
+      setCountries(data);
     } catch (e) {
       console.error(e.message);
     }
@@ -21,6 +24,27 @@ function App() {
     console.log("Fetching");
     fetchData();
   }, []);
+
+  const filterCountries = useCallback(() => {
+    const filteredCountry = [];
+    const startIndex = (currentPage - 1) * 10;
+    const endIndex = startIndex + 10;
+
+    for (let i = startIndex; i < endIndex && i < countries.length; i++) {
+      filteredCountry.push(countries[i]);
+    }
+    setFilteredCountries(filteredCountry);
+  }, [currentPage, countries]);
+
+  useEffect(() => {
+    filterCountries();
+  }, [filterCountries]);
+
+  const handleNext = () => {
+    if (currentPage * 10 < countries.length) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
 
   return (
     <>
@@ -33,7 +57,7 @@ function App() {
           <div className="z-10 flex justify-center">
             <div className="text-[#D2D5DA] bg-[#1B1D1F] border border-[#282B30] -mt-[60px] mx-3 rounded-xl py-6 px-3 md:px-7 md:mx-7 basis-7xl">
               <div className="grid grid-cols-1 md:grid-cols-[1fr_384px]  gap-6">
-                <span>Found 100 Countries</span>
+                <span>Found {countries.length} Countries</span>
                 <div className="relative">
                   <input
                     type="text"
@@ -110,6 +134,68 @@ function App() {
                         Independent
                       </label>
                     </div>
+                  </div>
+                </div>
+                <div className="w-full grow">
+                  <table className="w-full">
+                    <thead>
+                      <tr className="text-left text-xs">
+                        <td className=" pb-4 pr-3 border-b-2 border-[#282B30]">
+                          Flag
+                        </td>
+                        <td className=" pb-4 pr-3 border-b-2 border-[#282B30]">
+                          Name
+                        </td>
+                        <td className=" pb-4 pr-3 border-b-2 border-[#282B30]">
+                          Population
+                        </td>
+                        <td className=" pb-4 pr-3 border-b-2 border-[#282B30]">
+                          Area (km<sup>2</sup>)
+                        </td>
+                        <td className=" pb-4 pr-3 border-b-2 border-[#282B30]">
+                          Region
+                        </td>
+                      </tr>
+                    </thead>
+                    <tbody className="font-normal">
+                      {filteredCountries.map((country, i) => {
+                        return (
+                          <tr
+                            key={i}
+                            className="hover:bg-[#282B30] cursor-pointer transition-all"
+                          >
+                            <td className="p-3 pl-0">
+                              <img
+                                src={country.flags.png}
+                                alt={country.flags.alt}
+                                className="max-w-12 h-10 rounded-sm"
+                              />
+                            </td>
+                            <td className="p-3 pl-0">{country.name.common}</td>
+                            <td className="p-3 pl-0">
+                              {country.population.toLocaleString()}
+                            </td>
+                            <td className="p-3 pl-0">
+                              {country.area.toLocaleString()}
+                            </td>
+                            <td className="p-3 pl-0">{country.region}</td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                  <div className="w-full flex justify-between">
+                    <p className="text-sm self-end">
+                      Showing {(currentPage - 1) * 10 + 1} to{" "}
+                      {(currentPage - 1) * 10 + 10} of {countries.length}{" "}
+                      countries
+                    </p>
+                    <button
+                      className="px-3 py-1.5 border-2 border-[#282B30] rounded-xl hover:bg-[#282B30] cursor-pointer transition-all"
+                      onClick={handleNext}
+                    >
+                      Next Page {currentPage} »
+                    </button>
                   </div>
                 </div>
               </div>
